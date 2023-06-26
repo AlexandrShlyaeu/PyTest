a6dff6e7c4ed041a96ebc1c1825dba2ec85a76fd
Aliaksandr Shliayeu <aliaksandr_shliayeu@epam.com>
2023-06-26 11:11:52 +0300

    auth

---
+++ config/config.py
@@ -0,0 +1,25 @@
+class TestData:
+    CHROMEDRIVER_PATH = r'path_to_chromedriver'
+    BASE_URL = 'https://instagram.com'
+    USERNAME = 'username'
+    PASSWORD = 'password'
+    START_PAGE_TITLE = 'Instagram'
+    GOOGLE_PLAY_ICON_LINK = 'https://static.cdninstagram.com/rsrc.php/v3/yz/r/c5Rp7Ym-Klz.png'
+    MICROSOFT_ICON_LINK = 'https://static.cdninstagram.com/rsrc.php/v3/yu/r/EHY6QnZYdNX.png'
+    GOOGLE_PLAY_APP_LINK = 'https://l.instagram.com/?u=https%3A%2F%2Fplay.google.com%2Fstore%2Fapps%2Fdetails%3Fid%3Dcom.instagram.android%26referrer'
+    MICROSOFT_APP_LINK = 'ms-windows-store://pdp/?productid=9nblggh5l9xt&referrer=appbadge&source=www.instagram.com&mode=mini&pos=11%2C11%2C1313%2C997'
+    DONT_HAVE_AN_ACCOUNT_TITLE = "Don't have an account? Sign up"
+    GET_THE_APP_TITLE = 'Get the app.'
+    META_LINK = 'https://about.meta.com/'
+    ABOUT_LINK = 'https://about.instagram.com/'
+    LOGIN_FIELD_TITLE = 'Phone number, username, or email'
+    PASSWORD_FIELD_TITLE = 'Password'
+    LANGUAGE = ['Afrikaans', 'Čeština', 'Dansk', 'Ελληνικά', 'Хырмырдыр']
+    RESET_PASSWORD_LINK = 'https://www.instagram.com/accounts/password/reset/'
+    SIGN_UP_LINK = 'https://www.instagram.com/accounts/emailsignup/'
+    SIGN_UP_PAGE_TITLE = 'Sign up • Instagram'
+    RESET_PASSWORD_PAGE_TITLE = 'Reset Password • Instagram'
+    LOG_IN_WITH_FACEBOOK_PAGE_TITLE = 'Log into Facebook | Facebook'
+    META_PAGE_TITLE = 'Meta | Social Metaverse Company'
+
+
